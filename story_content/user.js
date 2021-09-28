73ed3df65cd4ea5075fd3eecb3607b870fd10f80
MadhanXC <91551365+MadhanXC@users.noreply.github.com>
2021-09-29 00:57:00 +0530

    Assessment

---
+++ story_content/user.js
@@ -0,0 +1,271 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5sukgzAgqHu":
+        Script1();
+        break;
+      case "6D955mRrMsK":
+        Script2();
+        break;
+      case "6p2co6yAhEf":
+        Script3();
+        break;
+      case "5nUYQlhxfky":
+        Script4();
+        break;
+      case "6BbGllVvZkf":
+        Script5();
+        break;
+      case "61xj3ajOzFk":
+        Script6();
+        break;
+      case "5p47KLW4TDP":
+        Script7();
+        break;
+      case "6jKYJIpCHYR":
+        Script8();
+        break;
+      case "6NNbicVpqzv":
+        Script9();
+        break;
+      case "6CPsYIAfshR":
+        Script10();
+        break;
+      case "5al86CPwlhS":
+        Script11();
+        break;
+      case "5WeC2kPFDwH":
+        Script12();
+        break;
+      case "5f32NeXbvM1":
+        Script13();
+        break;
+      case "5tW3hojrVcn":
+        Script14();
+        break;
+      case "6ENMcaxXHBk":
+        Script15();
+        break;
+      case "6cdNmY3noMx":
+        Script16();
+        break;
+      case "5tQ5hNFXSqI":
+        Script17();
+        break;
+      case "6glyKhxdEv6":
+        Script18();
+        break;
+      case "6XzpD3f5iL3":
+        Script19();
+        break;
+      case "6mWvgkiEs6r":
+        Script20();
+        break;
+      case "6e62xnV5iaT":
+        Script21();
+        break;
+      case "5scGVWxg08a":
+        Script22();
+        break;
+      case "5hmkn2JmJOX":
+        Script23();
+        break;
+      case "6X8kKpBIUiz":
+        Script24();
+        break;
+      case "6ny5tMwujH6":
+        Script25();
+        break;
+      case "6lgjWV9D0YB":
+        Script26();
+        break;
+      case "6NWP4Ug403X":
+        Script27();
+        break;
+      case "5vS4mG2W6Rs":
+        Script28();
+        break;
+      case "5dPsPgn1GHR":
+        Script29();
+        break;
+  }
+}
+
+function Script1()
+{
+  if(void 0===window.stencildatacloud){var player=GetPlayer(),targlink="https://cluelabs.com/stencil/display/widget-data-cloud-init?authtool=sl&v=1632855900&chart=NTgwfDI2Mzh8NTNmYTBkZjhkZWE0N2M4OGM0MWY0NTU0ODQxNzg2NzE";xhttp=new XMLHttpRequest,xhttp.onreadystatechange=function(){if(4==this.readyState&&200==this.status&&""!=this.responseText){var t=this.responseText,e=document.getElementsByTagName("head")[0],a=document.createElement("script");e.appendChild(a),a.appendChild(document.createTextNode(t)),window.stencildatacloud=!0}},xhttp.open("GET",targlink,!0),xhttp.send()}
+
+}
+
+function Script2()
+{
+  var player = GetPlayer();
+
+var entry = player.GetVar("ULOGINID");
+
+CloudSet('reflection', entry);
+}
+
+function Script3()
+{
+  if(void 0===window.stencildatacloud){var player=GetPlayer(),targlink="https://cluelabs.com/stencil/display/widget-data-cloud-init?authtool=sl&v=1632855900&chart=NTgwfDI2Mzh8NTNmYTBkZjhkZWE0N2M4OGM0MWY0NTU0ODQxNzg2NzE";xhttp=new XMLHttpRequest,xhttp.onreadystatechange=function(){if(4==this.readyState&&200==this.status&&""!=this.responseText){var t=this.responseText,e=document.getElementsByTagName("head")[0],a=document.createElement("script");e.appendChild(a),a.appendChild(document.createTextNode(t)),window.stencildatacloud=!0}},xhttp.open("GET",targlink,!0),xhttp.send()}
+
+}
+
+function Script4()
+{
+  if(void 0===window.stencildatacloud){var player=GetPlayer(),targlink="https://cluelabs.com/stencil/display/widget-data-cloud-init?authtool=sl&v=1632855900&chart=NTgwfDI2Mzh8NTNmYTBkZjhkZWE0N2M4OGM0MWY0NTU0ODQxNzg2NzE";xhttp=new XMLHttpRequest,xhttp.onreadystatechange=function(){if(4==this.readyState&&200==this.status&&""!=this.responseText){var t=this.responseText,e=document.getElementsByTagName("head")[0],a=document.createElement("script");e.appendChild(a),a.appendChild(document.createTextNode(t)),window.stencildatacloud=!0}},xhttp.open("GET",targlink,!0),xhttp.send()}
+
+}
+
+function Script5()
+{
+  if(void 0===window.stencildatacloud){var player=GetPlayer(),targlink="https://cluelabs.com/stencil/display/widget-data-cloud-init?authtool=sl&v=1632855900&chart=NTgwfDI2Mzh8NTNmYTBkZjhkZWE0N2M4OGM0MWY0NTU0ODQxNzg2NzE";xhttp=new XMLHttpRequest,xhttp.onreadystatechange=function(){if(4==this.readyState&&200==this.status&&""!=this.responseText){var t=this.responseText,e=document.getElementsByTagName("head")[0],a=document.createElement("script");e.appendChild(a),a.appendChild(document.createTextNode(t)),window.stencildatacloud=!0}},xhttp.open("GET",targlink,!0),xhttp.send()}
+
+}
+
+function Script6()
+{
+  if(void 0===window.stencildatacloud){var player=GetPlayer(),targlink="https://cluelabs.com/stencil/display/widget-data-cloud-init?authtool=sl&v=1632855900&chart=NTgwfDI2Mzh8NTNmYTBkZjhkZWE0N2M4OGM0MWY0NTU0ODQxNzg2NzE";xhttp=new XMLHttpRequest,xhttp.onreadystatechange=function(){if(4==this.readyState&&200==this.status&&""!=this.responseText){var t=this.responseText,e=document.getElementsByTagName("head")[0],a=document.createElement("script");e.appendChild(a),a.appendChild(document.createTextNode(t)),window.stencildatacloud=!0}},xhttp.open("GET",targlink,!0),xhttp.send()}
+
+}
+
+function Script7()
+{
+  if(void 0===window.stencildatacloud){var player=GetPlayer(),targlink="https://cluelabs.com/stencil/display/widget-data-cloud-init?authtool=sl&v=1632855900&chart=NTgwfDI2Mzh8NTNmYTBkZjhkZWE0N2M4OGM0MWY0NTU0ODQxNzg2NzE";xhttp=new XMLHttpRequest,xhttp.onreadystatechange=function(){if(4==this.readyState&&200==this.status&&""!=this.responseText){var t=this.responseText,e=document.getElementsByTagName("head")[0],a=document.createElement("script");e.appendChild(a),a.appendChild(document.createTextNode(t)),window.stencildatacloud=!0}},xhttp.open("GET",targlink,!0),xhttp.send()}
+
+}
+
+function Script8()
+{
+  if(void 0===window.stencildatacloud){var player=GetPlayer(),targlink="https://cluelabs.com/stencil/display/widget-data-cloud-init?authtool=sl&v=1632855900&chart=NTgwfDI2Mzh8NTNmYTBkZjhkZWE0N2M4OGM0MWY0NTU0ODQxNzg2NzE";xhttp=new XMLHttpRequest,xhttp.onreadystatechange=function(){if(4==this.readyState&&200==this.status&&""!=this.responseText){var t=this.responseText,e=document.getElementsByTagName("head")[0],a=document.createElement("script");e.appendChild(a),a.appendChild(document.createTextNode(t)),window.stencildatacloud=!0}},xhttp.open("GET",targlink,!0),xhttp.send()}
+
+}
+
+function Script9()
+{
+  if(void 0===window.stencildatacloud){var player=GetPlayer(),targlink="https://cluelabs.com/stencil/display/widget-data-cloud-init?authtool=sl&v=1632855900&chart=NTgwfDI2Mzh8NTNmYTBkZjhkZWE0N2M4OGM0MWY0NTU0ODQxNzg2NzE";xhttp=new XMLHttpRequest,xhttp.onreadystatechange=function(){if(4==this.readyState&&200==this.status&&""!=this.responseText){var t=this.responseText,e=document.getElementsByTagName("head")[0],a=document.createElement("script");e.appendChild(a),a.appendChild(document.createTextNode(t)),window.stencildatacloud=!0}},xhttp.open("GET",targlink,!0),xhttp.send()}
+
+}
+
+function Script10()
+{
+  if(void 0===window.stencildatacloud){var player=GetPlayer(),targlink="https://cluelabs.com/stencil/display/widget-data-cloud-init?authtool=sl&v=1632855900&chart=NTgwfDI2Mzh8NTNmYTBkZjhkZWE0N2M4OGM0MWY0NTU0ODQxNzg2NzE";xhttp=new XMLHttpRequest,xhttp.onreadystatechange=function(){if(4==this.readyState&&200==this.status&&""!=this.responseText){var t=this.responseText,e=document.getElementsByTagName("head")[0],a=document.createElement("script");e.appendChild(a),a.appendChild(document.createTextNode(t)),window.stencildatacloud=!0}},xhttp.open("GET",targlink,!0),xhttp.send()}
+
+}
+
+function Script11()
+{
+  if(void 0===window.stencildatacloud){var player=GetPlayer(),targlink="https://cluelabs.com/stencil/display/widget-data-cloud-init?authtool=sl&v=1632855900&chart=NTgwfDI2Mzh8NTNmYTBkZjhkZWE0N2M4OGM0MWY0NTU0ODQxNzg2NzE";xhttp=new XMLHttpRequest,xhttp.onreadystatechange=function(){if(4==this.readyState&&200==this.status&&""!=this.responseText){var t=this.responseText,e=document.getElementsByTagName("head")[0],a=document.createElement("script");e.appendChild(a),a.appendChild(document.createTextNode(t)),window.stencildatacloud=!0}},xhttp.open("GET",targlink,!0),xhttp.send()}
+
+}
+
+function Script12()
+{
+  if(void 0===window.stencildatacloud){var player=GetPlayer(),targlink="https://cluelabs.com/stencil/display/widget-data-cloud-init?authtool=sl&v=1632855900&chart=NTgwfDI2Mzh8NTNmYTBkZjhkZWE0N2M4OGM0MWY0NTU0ODQxNzg2NzE";xhttp=new XMLHttpRequest,xhttp.onreadystatechange=function(){if(4==this.readyState&&200==this.status&&""!=this.responseText){var t=this.responseText,e=document.getElementsByTagName("head")[0],a=document.createElement("script");e.appendChild(a),a.appendChild(document.createTextNode(t)),window.stencildatacloud=!0}},xhttp.open("GET",targlink,!0),xhttp.send()}
+
+}
+
+function Script13()
+{
+  if(void 0===window.stencildatacloud){var player=GetPlayer(),targlink="https://cluelabs.com/stencil/display/widget-data-cloud-init?authtool=sl&v=1632855900&chart=NTgwfDI2Mzh8NTNmYTBkZjhkZWE0N2M4OGM0MWY0NTU0ODQxNzg2NzE";xhttp=new XMLHttpRequest,xhttp.onreadystatechange=function(){if(4==this.readyState&&200==this.status&&""!=this.responseText){var t=this.responseText,e=document.getElementsByTagName("head")[0],a=document.createElement("script");e.appendChild(a),a.appendChild(document.createTextNode(t)),window.stencildatacloud=!0}},xhttp.open("GET",targlink,!0),xhttp.send()}
+
+}
+
+function Script14()
+{
+  if(void 0===window.stencildatacloud){var player=GetPlayer(),targlink="https://cluelabs.com/stencil/display/widget-data-cloud-init?authtool=sl&v=1632855900&chart=NTgwfDI2Mzh8NTNmYTBkZjhkZWE0N2M4OGM0MWY0NTU0ODQxNzg2NzE";xhttp=new XMLHttpRequest,xhttp.onreadystatechange=function(){if(4==this.readyState&&200==this.status&&""!=this.responseText){var t=this.responseText,e=document.getElementsByTagName("head")[0],a=document.createElement("script");e.appendChild(a),a.appendChild(document.createTextNode(t)),window.stencildatacloud=!0}},xhttp.open("GET",targlink,!0),xhttp.send()}
+
+}
+
+function Script15()
+{
+  if(void 0===window.stencildatacloud){var player=GetPlayer(),targlink="https://cluelabs.com/stencil/display/widget-data-cloud-init?authtool=sl&v=1632855900&chart=NTgwfDI2Mzh8NTNmYTBkZjhkZWE0N2M4OGM0MWY0NTU0ODQxNzg2NzE";xhttp=new XMLHttpRequest,xhttp.onreadystatechange=function(){if(4==this.readyState&&200==this.status&&""!=this.responseText){var t=this.responseText,e=document.getElementsByTagName("head")[0],a=document.createElement("script");e.appendChild(a),a.appendChild(document.createTextNode(t)),window.stencildatacloud=!0}},xhttp.open("GET",targlink,!0),xhttp.send()}
+
+}
+
+function Script16()
+{
+  if(void 0===window.stencildatacloud){var player=GetPlayer(),targlink="https://cluelabs.com/stencil/display/widget-data-cloud-init?authtool=sl&v=1632855900&chart=NTgwfDI2Mzh8NTNmYTBkZjhkZWE0N2M4OGM0MWY0NTU0ODQxNzg2NzE";xhttp=new XMLHttpRequest,xhttp.onreadystatechange=function(){if(4==this.readyState&&200==this.status&&""!=this.responseText){var t=this.responseText,e=document.getElementsByTagName("head")[0],a=document.createElement("script");e.appendChild(a),a.appendChild(document.createTextNode(t)),window.stencildatacloud=!0}},xhttp.open("GET",targlink,!0),xhttp.send()}
+
+}
+
+function Script17()
+{
+  if(void 0===window.stencildatacloud){var player=GetPlayer(),targlink="https://cluelabs.com/stencil/display/widget-data-cloud-init?authtool=sl&v=1632855900&chart=NTgwfDI2Mzh8NTNmYTBkZjhkZWE0N2M4OGM0MWY0NTU0ODQxNzg2NzE";xhttp=new XMLHttpRequest,xhttp.onreadystatechange=function(){if(4==this.readyState&&200==this.status&&""!=this.responseText){var t=this.responseText,e=document.getElementsByTagName("head")[0],a=document.createElement("script");e.appendChild(a),a.appendChild(document.createTextNode(t)),window.stencildatacloud=!0}},xhttp.open("GET",targlink,!0),xhttp.send()}
+
+}
+
+function Script18()
+{
+  if(void 0===window.stencildatacloud){var player=GetPlayer(),targlink="https://cluelabs.com/stencil/display/widget-data-cloud-init?authtool=sl&v=1632855900&chart=NTgwfDI2Mzh8NTNmYTBkZjhkZWE0N2M4OGM0MWY0NTU0ODQxNzg2NzE";xhttp=new XMLHttpRequest,xhttp.onreadystatechange=function(){if(4==this.readyState&&200==this.status&&""!=this.responseText){var t=this.responseText,e=document.getElementsByTagName("head")[0],a=document.createElement("script");e.appendChild(a),a.appendChild(document.createTextNode(t)),window.stencildatacloud=!0}},xhttp.open("GET",targlink,!0),xhttp.send()}
+
+}
+
+function Script19()
+{
+  if(void 0===window.stencildatacloud){var player=GetPlayer(),targlink="https://cluelabs.com/stencil/display/widget-data-cloud-init?authtool=sl&v=1632855900&chart=NTgwfDI2Mzh8NTNmYTBkZjhkZWE0N2M4OGM0MWY0NTU0ODQxNzg2NzE";xhttp=new XMLHttpRequest,xhttp.onreadystatechange=function(){if(4==this.readyState&&200==this.status&&""!=this.responseText){var t=this.responseText,e=document.getElementsByTagName("head")[0],a=document.createElement("script");e.appendChild(a),a.appendChild(document.createTextNode(t)),window.stencildatacloud=!0}},xhttp.open("GET",targlink,!0),xhttp.send()}
+
+}
+
+function Script20()
+{
+  if(void 0===window.stencildatacloud){var player=GetPlayer(),targlink="https://cluelabs.com/stencil/display/widget-data-cloud-init?authtool=sl&v=1632855900&chart=NTgwfDI2Mzh8NTNmYTBkZjhkZWE0N2M4OGM0MWY0NTU0ODQxNzg2NzE";xhttp=new XMLHttpRequest,xhttp.onreadystatechange=function(){if(4==this.readyState&&200==this.status&&""!=this.responseText){var t=this.responseText,e=document.getElementsByTagName("head")[0],a=document.createElement("script");e.appendChild(a),a.appendChild(document.createTextNode(t)),window.stencildatacloud=!0}},xhttp.open("GET",targlink,!0),xhttp.send()}
+
+}
+
+function Script21()
+{
+  if(void 0===window.stencildatacloud){var player=GetPlayer(),targlink="https://cluelabs.com/stencil/display/widget-data-cloud-init?authtool=sl&v=1632855900&chart=NTgwfDI2Mzh8NTNmYTBkZjhkZWE0N2M4OGM0MWY0NTU0ODQxNzg2NzE";xhttp=new XMLHttpRequest,xhttp.onreadystatechange=function(){if(4==this.readyState&&200==this.status&&""!=this.responseText){var t=this.responseText,e=document.getElementsByTagName("head")[0],a=document.createElement("script");e.appendChild(a),a.appendChild(document.createTextNode(t)),window.stencildatacloud=!0}},xhttp.open("GET",targlink,!0),xhttp.send()}
+
+}
+
+function Script22()
+{
+  if(void 0===window.stencildatacloud){var player=GetPlayer(),targlink="https://cluelabs.com/stencil/display/widget-data-cloud-init?authtool=sl&v=1632855900&chart=NTgwfDI2Mzh8NTNmYTBkZjhkZWE0N2M4OGM0MWY0NTU0ODQxNzg2NzE";xhttp=new XMLHttpRequest,xhttp.onreadystatechange=function(){if(4==this.readyState&&200==this.status&&""!=this.responseText){var t=this.responseText,e=document.getElementsByTagName("head")[0],a=document.createElement("script");e.appendChild(a),a.appendChild(document.createTextNode(t)),window.stencildatacloud=!0}},xhttp.open("GET",targlink,!0),xhttp.send()}
+
+}
+
+function Script23()
+{
+  if(void 0===window.stencildatacloud){var player=GetPlayer(),targlink="https://cluelabs.com/stencil/display/widget-data-cloud-init?authtool=sl&v=1632855900&chart=NTgwfDI2Mzh8NTNmYTBkZjhkZWE0N2M4OGM0MWY0NTU0ODQxNzg2NzE";xhttp=new XMLHttpRequest,xhttp.onreadystatechange=function(){if(4==this.readyState&&200==this.status&&""!=this.responseText){var t=this.responseText,e=document.getElementsByTagName("head")[0],a=document.createElement("script");e.appendChild(a),a.appendChild(document.createTextNode(t)),window.stencildatacloud=!0}},xhttp.open("GET",targlink,!0),xhttp.send()}
+
+}
+
+function Script24()
+{
+  if(void 0===window.stencildatacloud){var player=GetPlayer(),targlink="https://cluelabs.com/stencil/display/widget-data-cloud-init?authtool=sl&v=1632855900&chart=NTgwfDI2Mzh8NTNmYTBkZjhkZWE0N2M4OGM0MWY0NTU0ODQxNzg2NzE";xhttp=new XMLHttpRequest,xhttp.onreadystatechange=function(){if(4==this.readyState&&200==this.status&&""!=this.responseText){var t=this.responseText,e=document.getElementsByTagName("head")[0],a=document.createElement("script");e.appendChild(a),a.appendChild(document.createTextNode(t)),window.stencildatacloud=!0}},xhttp.open("GET",targlink,!0),xhttp.send()}
+
+}
+
+function Script25()
+{
+  if(void 0===window.stencildatacloud){var player=GetPlayer(),targlink="https://cluelabs.com/stencil/display/widget-data-cloud-init?authtool=sl&v=1632855900&chart=NTgwfDI2Mzh8NTNmYTBkZjhkZWE0N2M4OGM0MWY0NTU0ODQxNzg2NzE";xhttp=new XMLHttpRequest,xhttp.onreadystatechange=function(){if(4==this.readyState&&200==this.status&&""!=this.responseText){var t=this.responseText,e=document.getElementsByTagName("head")[0],a=document.createElement("script");e.appendChild(a),a.appendChild(document.createTextNode(t)),window.stencildatacloud=!0}},xhttp.open("GET",targlink,!0),xhttp.send()}
+
+}
+
+function Script26()
+{
+  if(void 0===window.stencildatacloud){var player=GetPlayer(),targlink="https://cluelabs.com/stencil/display/widget-data-cloud-init?authtool=sl&v=1632855900&chart=NTgwfDI2Mzh8NTNmYTBkZjhkZWE0N2M4OGM0MWY0NTU0ODQxNzg2NzE";xhttp=new XMLHttpRequest,xhttp.onreadystatechange=function(){if(4==this.readyState&&200==this.status&&""!=this.responseText){var t=this.responseText,e=document.getElementsByTagName("head")[0],a=document.createElement("script");e.appendChild(a),a.appendChild(document.createTextNode(t)),window.stencildatacloud=!0}},xhttp.open("GET",targlink,!0),xhttp.send()}
+
+}
+
+function Script27()
+{
+  if(void 0===window.stencildatacloud){var player=GetPlayer(),targlink="https://cluelabs.com/stencil/display/widget-data-cloud-init?authtool=sl&v=1632855900&chart=NTgwfDI2Mzh8NTNmYTBkZjhkZWE0N2M4OGM0MWY0NTU0ODQxNzg2NzE";xhttp=new XMLHttpRequest,xhttp.onreadystatechange=function(){if(4==this.readyState&&200==this.status&&""!=this.responseText){var t=this.responseText,e=document.getElementsByTagName("head")[0],a=document.createElement("script");e.appendChild(a),a.appendChild(document.createTextNode(t)),window.stencildatacloud=!0}},xhttp.open("GET",targlink,!0),xhttp.send()}
+
+}
+
+function Script28()
+{
+  if(void 0===window.stencildatacloud){var player=GetPlayer(),targlink="https://cluelabs.com/stencil/display/widget-data-cloud-init?authtool=sl&v=1632855900&chart=NTgwfDI2Mzh8NTNmYTBkZjhkZWE0N2M4OGM0MWY0NTU0ODQxNzg2NzE";xhttp=new XMLHttpRequest,xhttp.onreadystatechange=function(){if(4==this.readyState&&200==this.status&&""!=this.responseText){var t=this.responseText,e=document.getElementsByTagName("head")[0],a=document.createElement("script");e.appendChild(a),a.appendChild(document.createTextNode(t)),window.stencildatacloud=!0}},xhttp.open("GET",targlink,!0),xhttp.send()}
+
+}
+
+function Script29()
+{
+  if(void 0===window.stencildatacloud){var player=GetPlayer(),targlink="https://cluelabs.com/stencil/display/widget-data-cloud-init?authtool=sl&v=1632855900&chart=NTgwfDI2Mzh8NTNmYTBkZjhkZWE0N2M4OGM0MWY0NTU0ODQxNzg2NzE";xhttp=new XMLHttpRequest,xhttp.onreadystatechange=function(){if(4==this.readyState&&200==this.status&&""!=this.responseText){var t=this.responseText,e=document.getElementsByTagName("head")[0],a=document.createElement("script");e.appendChild(a),a.appendChild(document.createTextNode(t)),window.stencildatacloud=!0}},xhttp.open("GET",targlink,!0),xhttp.send()}
+
+}
+
